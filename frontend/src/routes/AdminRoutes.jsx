545dc6aab3015c2e93fb9869dba821b3c4a621f6
xXIOrehIXx <tranhunganhC@gmail.com>
2026-03-20 20:59:01 +0700

    frontend: implement admin menu items
    CRUD for menu items

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import AdminMenuCategory from "../component/pages/Menu/AdminmenuCategory";
+import AdminMenuItem from "../component/pages/Menu/AdminmenuItem";
 
 const AdminRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="item" />} />
       <Route path="category" element={<AdminMenuCategory />} />
+      <Route path="item" element={<AdminMenuItem />} />
     </Routes>
   );
 };
