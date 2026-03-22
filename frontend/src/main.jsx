bf0a6f847ef1429acda2077466a6b1ba0b53bb16
xXIOrehIXx <tranhunganhC@gmail.com>
2026-03-22 14:50:52 +0700

    frontend: implement admin table menu
    + Add a menu to allow admin to CRUD restaurant tables

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 window.onerror = (msg, src, line, col, err) => {
   document.body.innerHTML = `<pre style="color:red;font-size:16px">${msg}\n${src}:${line}\n${err?.stack}</pre>`;
--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import AdminMenuCategory from "../component/pages/Menu/AdminmenuCategory";
 import AdminMenuItem from "../component/pages/Menu/AdminmenuItem";
+import AdminTableQRPage from "../component/pages/ManagementTable/AdminTableQRPage";
 
 const AdminRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="item" />} />
+      <Route path="tables" element={<AdminTableQRPage />} />
       <Route path="category" element={<AdminMenuCategory />} />
       <Route path="item" element={<AdminMenuItem />} />
     </Routes>
